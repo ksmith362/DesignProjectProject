9d9ab9e496ee6a454dc66091db46c8911e004aa9
ksmith362 <j02251542@jwu.edu>
2022-01-22 22:37:50 -0500

    Initial Commit

---
+++ Twitter/TwitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterApp.swift
+//  Twitter
+//
+//  Created by Kate Smith on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
