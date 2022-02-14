e71ba61b83c41d0bdbd2731096c49a8f7b41c496
ksmith362 <j02251542@jwu.edu>
2022-02-14 12:38:47 -0500

    started creating the html for local host and php stuff

---
+++ Twitter.php
@@ -0,0 +1,45 @@
+
+<html>
+<head>
+<style>
+	.center{ 
+		text-align: center;
+    background-color: Violet;
+		color: white;}
+
+</style>
+</head>
+	<body>
+
+<h1 class="center">Twitter</h1>
+<img src = "TwitterLogo.png" alt = "Twitter Logo"
+  width = "100"
+  height = "150"
+  class = "floatRight">
+
+		
+
+<form action = "Welcome.php" method = "POST">
+
+
+			First Name: <input type= "text" name = "firstname" ><br ><br />
+			Username: <input  type = "text" name = "username" ><br ><br />
+			Password: <input type= "text" name = "password" ><br ><br ><br />
+
+			
+
+<input type="submit" value="Bing Bong">
+			
+
+
+</form>
+
+
+
+</body>
+
+</html>
+
+
+
+
---
+++ Welcome.php
@@ -0,0 +1,27 @@
+
+<html>
+	<head>
+		<title>
+			Twitter
+		</title>
+	</head>
+	What's Up BITCHEEESSS
+	<body>
+
+		<form 
+
+			
+
+			
+
+			<input 
+
+		</form>
+
+
+
+
+	</body>
+
+</html>
+
